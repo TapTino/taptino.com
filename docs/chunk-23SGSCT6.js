55227147ef4125e5b433566912cad82bb8e67e3e
Crystal Spider <riccardo.chimisso@gmail.com>
2025-05-21 18:00:37 +0200

    Init draft.

---
+++ docs/chunk-23SGSCT6.js
@@ -0,0 +1,2 @@
+import{o as a}from"./chunk-GYEGRVHU.js";import{Xb as i,cc as r,pa as o}from"./chunk-DHE45RMH.js";var f=(()=>{let e=class e{};e.\u0275fac=function(n){return new(n||e)},e.\u0275cmp=o({type:e,selectors:[["tpt-news"]],standalone:!0,features:[r],decls:1,vars:0,template:function(n,p){n&1&&i(0,`CIAO!
+`)},dependencies:[a]});let t=e;return t})();export{f as NewsletterComponent};
